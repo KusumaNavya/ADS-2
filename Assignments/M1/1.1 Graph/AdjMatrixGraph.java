033b58e84805b20d3049a93895290f6092388c2f
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-11-05 16:44:34 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/M1/1.1 Graph/AdjMatrixGraph.java
+++ Assignments/M1/1.1 Graph/AdjMatrixGraph.java
@@ -1,57 +1,102 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
-
+/**
+ * Class for adj matrix graph.
+ */
 public class AdjMatrixGraph {
+    /**
+     * { var_description }
+     */
     private static final String NEWLINE = System.getProperty("line.separator");
-
+    /**
+     * { var_description }
+     */
     private final int V;
+    /**
+     * { var_description }
+     */
     private int E;
+    /**
+     * { var_description }
+     */
     private boolean[][] adj;
-
+    //
     // empty graph with V vertices
+    //
+    // @param      V     { parameter_description }
+    //
     public AdjMatrixGraph(int V) {
         if (V < 0) throw new IllegalArgumentException("Too few vertices");
         this.V = V;
         this.E = 0;
         this.adj = new boolean[V][V];
     }
-
-    // number of vertices and edges
+    /**
+     * { function_description }
+     *    // number of vertices and edges
+     * @return     { description_of_the_return_value }
+     */
     public int V() { return V; }
     public int E() { return E; }
-
-
-    // add undirected edge v-w
+    /**
+     * Adds an edge.
+     *    // add undirected edge v-w
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
     public void addEdge(int v, int w) {
         if (!adj[v][w]) E++;
         adj[v][w] = true;
         adj[w][v] = true;
     }
-
-    // does the graph contain the edge v-w?
+    /**
+     * Determines if it has edge.
+     *    // does the graph contain the edge v-w?
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
     public boolean hasEdge(int v, int w) {
         return adj[v][w];
     }
-
-    // return list of neighbors of v
+    /**
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterable<Integer> adj(int v) {
         return new AdjIterator(v);
     }
-
-    // support iteration over graph vertices
+    /**
+     * Class for adj iterator.
+     */
     private class AdjIterator implements Iterator<Integer>, Iterable<Integer> {
         private int v;
         private int w = 0;
-
+        /**
+         * Constructs the object.
+         *
+         * @param      v     { parameter_description }
+         */
         AdjIterator(int v) {
             this.v = v;
         }
-
+        /**
+         * { function_description }
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Iterator<Integer> iterator() {
             return this;
         }
-
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
         public boolean hasNext() {
             while (w < V) {
                 if (adj[v][w]) return true;
@@ -59,21 +104,29 @@ public class AdjMatrixGraph {
             }
             return false;
         }
-
+        /**
+         * { function_description }
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Integer next() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
             return w++;
         }
-
+        /**
+         * { function_description }
+         */
         public void remove()  {
             throw new UnsupportedOperationException();
         }
     }
-
-
-    // string representation of Graph - takes quadratic time
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String display() {
         String s = "";
         s += V + " vertices, " + E + " edges" + NEWLINE;
